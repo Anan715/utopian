216c4d2daaf2dc0ac2fdaad003cc6523372a6320
Anan715 <hufulai001@sina.com>
2023-05-12 19:16:12 +0800

    RedisDelayQueue

--- rbac/src/main/java/com/utopian/tech/rbac/controller/RedisDelayQueueTestController.java
+++ rbac/src/main/java/com/utopian/tech/rbac/controller/RedisDelayQueueTestController.java
@@ -2,7 +2,6 @@ package com.utopian.tech.rbac.controller;
 
 import com.utopian.tech.base.exception.UtopianException;
 import com.utopian.tech.base.response.UtopianResponse;
-import com.utopian.tech.base.util.RedisUtil;
 import com.utopian.tech.rbac.demo.RedisDelayQueueService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
